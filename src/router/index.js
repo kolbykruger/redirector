cc1a86596dd83b7356ca43af61ac811977493f9d
Kolby Kruger <kolbykruger@gmail.com>
2020-04-28 00:51:34 -0400

    added router views for each stage

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,47 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+},
+  {
+    path: '/stage-1',
+    name: 'Stage 1',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Stage-1.vue')
+},
+{
+  path: '/stage-2',
+  name: 'Stage 2',
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  component: () => import(/* webpackChunkName: "about" */ '../views/Stage-2.vue')
+},
+{
+  path: '/stage-3',
+  name: 'Stage 3',
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  component: () => import(/* webpackChunkName: "about" */ '../views/Stage-3.vue')
+},
+{
+  path: '/stage-4',
+  name: 'Stage 4',
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  component: () => import(/* webpackChunkName: "about" */ '../views/Stage-4.vue')
+},
+{
+  path: '/stage-5',
+  name: 'Stage 5',
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  component: () => import(/* webpackChunkName: "about" */ '../views/Stage-5.vue')
+}
 ]
 
 const router = new VueRouter({
