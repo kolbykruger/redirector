5dcf116b61f2bc0085e40cfd74e528fbd7a14f7b
Kolby Kruger <kolbykruger@gmail.com>
2020-04-30 14:04:03 -0400

    added indicator component

---
+++ src/components/Indicator.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+    <div class="indicator" :class="{'indicator-true': indicatorData.status}">
+        <div class="circle">
+            <div class="icon" v-if="indicatorData.status">
+                ✔
+            </div>
+            <div class="icon" v-else>
+                ✖
+            </div>
+        </div>
+        <span class="indicator-tooltip">{{ indicatorData.stage }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Indicator',
+    props: ['indicatorData']
+}
+</script>
