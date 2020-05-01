7a2f36832746b10b1801ad02a97f5b5ed73bf4de
Kolby Kruger <kolbykruger@gmail.com>
2020-05-01 11:02:20 -0400

    properly fixed scroll behavior between routes

--- src/router/index.js
+++ src/router/index.js
@@ -52,7 +52,10 @@ Vue.use(VueRouter)
 
 const router = new VueRouter({
     mode: 'history',
-  routes
+    routes,
+    scrollBehavior() {
+        document.getElementById('app').scrollIntoView();
+    }
 })
 
 export default router
