2ecc5004ab5c291523ee1a50ca0f985c27936970
Kolby Kruger <kolbykruger@gmail.com>
2020-05-01 01:35:18 -0400

    build 0.5

---
+++ dist/js/about.1e1d4280.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(e,t,a){"use strict";a.r(t);var n=function(){var e=this,t=e.$createElement,a=e._self._c||t;return a("div",[a("PageHeading",{attrs:{title:"About Redirector",summary:""}})],1)},o=[],c=a("18dc"),r={name:"about",components:{PageHeading:c["a"]}},u=r,i=a("2877"),s=Object(i["a"])(u,n,o,!1,null,"54379ca6",null);t["default"]=s.exports}}]);
+//# sourceMappingURL=about.1e1d4280.js.map
