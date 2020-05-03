7879014d9be74f63c596d94aa3f5f34b7a23de82
Kolby Kruger <kolbykruger@gmail.com>
2020-05-03 18:47:06 -0400

    created

---
+++ src/components/PartialMatchOptionLink.vue
@@ -0,0 +1,32 @@
+<template>
+  <button class="link link-draggable"
+    @click.exact="selection(link)"
+    @click.ctrl.exact="$emit('linkData', link)">
+     <span>{{ link.pathname }}</span>
+ </button>
+</template>
+
+<script>
+export default {
+  name: 'PartialMatchOptionLink',
+  props: {
+      link: Object,
+      selected: Boolean,
+  },
+  data() {
+      return {
+          isSelected: false,
+      }
+  },
+  methods: {
+      selection(link) {
+          this.$emit('selectLink', link);
+      }
+  },
+  created() {
+      if (this.selected) {
+          this.$emit('optionSelected', this.link);
+      }
+  }
+}
+</script>
