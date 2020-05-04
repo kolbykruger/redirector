9770a81c94a90b5bcf822a3fdaa4f25fa1fb2322
Kolby Kruger <kolbykruger@gmail.com>
2020-05-03 22:16:16 -0400

    styled/created home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,42 @@
 <template>
   <div class="home">
 
-    <PageHeading title="Welcome to Redirector!" summary="The tool designed to help you bulk create 301 redirects."/>
-    <section>
-        <div class="container">
-            <p>We currently offer <strong>Apache (.htaccess)</strong> and <strong>Nginx (nginx.conf)</strong> outputs.</p>
-        </div>
-    </section>
-    <section class="start-app">
-        <div class="container">
-            <br>
-            <div class="group buttons">
-                <router-link class="button button-green button-large" to="/stage-0">Start making redirects -></router-link>
+    <div class="homescreen">
+        <div class="white">
+            <div class="content">
+                <h1>redirector</h1>
+                <p>A tool for creating bulk redirects.</p>
+                <div class="buttons buttons-center">
+                    <router-link class="button button-green button-large" to="/stage-0">Get started -></router-link>
+                </div>
             </div>
         </div>
-    </section>
+
+        <div class="black">
+            <div class="content">
+                <h1>redirector</h1>
+                <p>A tool for creating bulk redirects.</p>
+                <div class="buttons buttons-center">
+                    <router-link class="button button-green button-large" to="/stage-0">Get started -></router-link>
+                </div>
+            </div>
+        </div>
+    </div>
 
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import PageHeading from '@/components/PageHeading.vue'
 
 export default {
   name: 'Home',
-  components: {
-    PageHeading
+  data() {
+      return {
+        isVisible: false,
+      }
+  },
+  mounted() {
+      this.isVisible = true
   }
 }
 </script>
