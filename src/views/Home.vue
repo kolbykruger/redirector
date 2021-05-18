07575e10a44e206f4e4f6f165a4f88e9ceefee18
Kolby Kruger <kolbykruger@gmail.com>
2021-05-17 22:31:46 -0400

    added beta button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,42 +1,71 @@
 <template>
-  <div class="home">
-
-    <div class="homescreen">
-        <div class="white">
-            <div class="content">
-                <h1>redirector</h1>
-                <p>A tool for creating bulk redirects.</p>
-                <div class="buttons buttons-center">
-                    <router-link class="button button-green button-large" to="/stage-0">Get started -></router-link>
+    <div class="home">
+        <div class="homescreen">
+            <div class="white">
+                <div class="content">
+                    <h1>redirector</h1>
+                    <p>A tool for creating bulk redirects.</p>
+                    <div class="buttons buttons-center">
+                        <router-link
+                            class="button button-green button-large"
+                            to="/stage-0"
+                            >Get started -></router-link
+                        >
+                    </div>
+                    <div class="buttons buttons-center">
+                        <a
+                            href="https://redirector-2.netlify.app/"
+                            target="_blank"
+                            rel="nofollow norefer"
+                            class="button button-beta"
+                            >Try our new Beta version</a
+                        >
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="black">
-            <div class="content">
-                <h1>redirector</h1>
-                <p>A tool for creating bulk redirects.</p>
-                <div class="buttons buttons-center">
-                    <router-link class="button button-green button-large" to="/stage-0">Get started -></router-link>
+            <div class="black">
+                <div class="content">
+                    <h1>redirector</h1>
+                    <p>A tool for creating bulk redirects.</p>
+                    <div class="buttons buttons-center">
+                        <router-link
+                            class="button button-green button-large"
+                            to="/stage-0"
+                            >Get started -></router-link
+                        >
+                    </div>
+                    <div class="buttons buttons-center">
+                        <a
+                            href="https://redirector-2.netlify.app/"
+                            target="_blank"
+                            rel="nofollow norefer"
+                            class="button button-beta"
+                            >Try our new Beta version</a
+                        >
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-
-  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'Home',
-  data() {
-      return {
-        isVisible: false,
-      }
-  },
-  mounted() {
-      this.isVisible = true
-  }
+    name: 'Home',
+    data() {
+        return {
+            isVisible: false
+        }
+    },
+    mounted() {
+        this.isVisible = true
+    }
 }
 </script>
+
+<style lang="scss">
+.buttons a.button.button-beta {
+    background: rgb(234, 68, 57);
+}
+</style>
